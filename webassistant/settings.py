9166849a2b29617500d8d9b0efa576dc1ee5d267
David Huy Nguyen <davidNg@gmail.com>
2023-08-06 02:18:16 +0700

    allow vecel host

--- webassistant/settings.py
+++ webassistant/settings.py
@@ -43,7 +43,7 @@ SECRET_KEY = 'z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 # 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 # ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 # Application definition
