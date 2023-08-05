d7017c945bfa9be96be61f8001011854f73f28d9
David Huy Nguyen <davidNg@gmail.com>
2023-08-06 03:20:23 +0700

    allow vecel host

--- build_files.sh
+++ build_files.sh
@@ -3,8 +3,8 @@ pip install -r requirements.txt
 pip install djongo
 pip install pytz
 pip install dnspython
-pip uninstall pymongo
-Y
+yes | pip uninstall pymongo
+
 python3.9 manage.py collectstatic
 python3.9 manage.py runserver 
 
